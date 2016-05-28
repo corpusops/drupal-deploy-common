52ab3b7a3bcef0586463cd5ba56090cde988cf4e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-05-28 21:04:27 +0100

    finish certbot renewal

--- .ansible/playbooks/roles/drupal/roles/haproxy/setup/certbot/deploy.sh
+++ .ansible/playbooks/roles/drupal/roles/haproxy/setup/certbot/deploy.sh
@@ -4,15 +4,6 @@
 . "$(dirname $(readlink -f $0))/base"
 if [ "x${SDEBUG-}" != "x" ];then set -x;fi
 LIVEFOLDER=${LIVEFOLDER:-/etc/letsencrypt/live}
-for f in $(find -L /certificates -name "*.crt" -type f -not -name self.crt 2>/dev/null );do
-    log "handling $f"
-    echo "new ssl cert $f" \
-        | nc $HAPROXY_INT_IP $HAPROXY_CPORT
-    echo -e -n "set ssl cert $f <<\n$(cat $f)\n\n" \
-        | nc $HAPROXY_INT_IP $HAPROXY_CPORT
-    echo "commit ssl cert $f" \
-        | nc $HAPROXY_INT_IP $HAPROXY_CPORT
-    echo "show ssl cert $f" \
-        | nc $HAPROXY_INT_IP $HAPROXY_CPORT |egrep -i "^(subject|not|filename)"
-done
+# for now nothing has to be done
+exit 0
 # vim:set et sts=4 ts=4 tw=0:
--- .ansible/playbooks/roles/drupal/roles/haproxy/setup/certbot/post.sh
+++ .ansible/playbooks/roles/drupal/roles/haproxy/setup/certbot/post.sh
@@ -2,6 +2,7 @@
 . "$(dirname $(readlink -f $0))/base"
 if [ "x${SDEBUG-}" != "x" ];then set -x;fi
 LIVEFOLDER=${LIVEFOLDER:-/etc/letsencrypt/live}
+# transform certs in haproxy format
 for f in $(find -L $LIVEFOLDER -name fullchain.pem 2>/dev/null );do
     d=$(dirname $f)
     c=$(basename $d)
@@ -12,5 +13,17 @@ for f in $(find -L $LIVEFOLDER -name fullchain.pem 2>/dev/null );do
             cp $d/full.pem /certificates/$n
         fi
     fi
+done
+# reload haproxy conf
+for f in $(find -L /certificates -name "*.crt" -type f -not -name self.crt 2>/dev/null );do
+    log "handling $f"
+    echo "new ssl cert $f" \
+        | nc $HAPROXY_INT_IP $HAPROXY_CPORT
+    echo -e -n "set ssl cert $f <<\n$(cat $f)\n\n" \
+        | nc $HAPROXY_INT_IP $HAPROXY_CPORT
+    echo "commit ssl cert $f" \
+        | nc $HAPROXY_INT_IP $HAPROXY_CPORT
+    echo "show ssl cert $f" \
+        | nc $HAPROXY_INT_IP $HAPROXY_CPORT |egrep -i "^(subject|not|filename)"
 done
 # vim:set et sts=4 ts=4 tw=0:
