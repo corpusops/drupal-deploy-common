519664ceac4f9befaad3c604cd0d35433ef90bfb
regilero <regis.leroy@makina-corpus.com>
2020-10-14 11:19:07 +0200

    Doc generation: try to avoid root ownership on result

--- docs/entry.sh
+++ docs/entry.sh
@@ -6,8 +6,12 @@ BUILDDIR=${BUILDDIR:-"/code/app.host/var/private/docs"}
 NO_INIT=${NO_INIT-}
 NO_CLEAN=${NO_CLEAN-}
 NO_HTML=${NO_HTML-}
+OWNER=${HOST_USER_UID:-1000}
 init() {
-	if [ ! -e "$BUILDDIR" ];then mkdir -pv "$BUILDDIR";fi
+	if [ ! -e "$BUILDDIR" ];then
+        mkdir -pv "$BUILDDIR";
+        chown ${OWNER} "$BUILDDIR";
+    fi
 }
 [[ -z ${NO_INIT} ]] && init
 if [[ -n "$@" ]];then
@@ -16,3 +20,5 @@ else
     [[ -z ${NO_CLEAN} ]] && make clean
     [[ -z ${NO_HTML} ]] && make html
 fi
+
+chown -R ${OWNER} "$BUILDDIR";
