90853fab3e4e4d2d9ef45d0982f3008e93094950
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2021-06-19 21:12:39 +0100

    More intelligent composer wrappers

--- sys/sbin/composerinstall.sh
+++ sys/sbin/composerinstall.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 SDEBUG=${SDEBUG-}
 SCRIPTSDIR="$(dirname $(readlink -f "$0"))"
-SHELL_USER=${SHELL_USER-}
-cd "$SCRIPTSDIR/../.."
-TOPDIR=$(pwd)
+SHELL_USER=${SHELL_USER-$(whoami)}
+COMPOSER_JSON_CANDIDATES=${COMPOSER_JSON_CANDIDATES:-app/composer.json composer.json}
+TOPDIR_CANDIDATES=${TOPDIR_CANDIDATES:-$SCRIPTSDIR/../.. $SCRIPTSDIR/../../../../ ../}
+
+# detect root folder, either 4 levels under if called from common-glue
+# or 2 from project root
+TOPDIR=
+for i in $TOPDIR_CANDIDATES;do
+    for j in $COMPOSER_JSON_CANDIDATES;do
+        if [ -e "$i/$j" ];then
+            cd "$i"
+            TOPDIR=$(pwd)
+            break
+        fi
+    done
+done
+if [[ -z "$TOPDIR" ]];then
+    echo "Can't detect project root level" >&2
+    exit 1
+fi
+
+cd "$TOPDIR"
 
 # now be in stop-on-error mode
 set -e
@@ -27,20 +46,24 @@ export APP_TYPE="${APP_TYPE:-drupal}"
 export APP_USER="${APP_USER:-$APP_TYPE}"
 export APP_GROUP="$APP_USER"
 
+
 if [ "x${SHELL_USER}" = "x${APP_USER}" ]; then
     GOSU_CMD=""
 else
     GOSU_CMD="gosu $APP_USER"
 fi
 
-if [ -e $SCRIPTSDIR/pre-composer.sh ]; then
-    $SCRIPTSDIR/pre-composer.sh
-fi
+for i in "$TOPDIR" "$TOPDIR/scripts" "$SCRIPTSDIR";do
+    if [ -e $i/pre-composer.sh ]; then
+        $i/pre-composer.sh
+        break
+    fi
+done
 (
     cd $PROJECT_DIR \
     && $GOSU_CMD /usr/local/bin/composer clear-cache \
-    && $GOSU_CMD /usr/local/bin/composer install  --prefer-dist --optimize-autoloader --no-interaction --verbose $@
+    && $GOSU_CMD echo "afwully disabling tls, seems CentOS+TLS is bad for https://codeload.github.com" \
+    && $GOSU_CMD /usr/local/bin/composer config -g disable-tls true \
+    && $GOSU_CMD sh -c 'COMPOSER_MEMORY_LIMIT=-1 php -d memory_limit=-1 \
+        /usr/local/bin/composer install  --prefer-dist --optimize-autoloader --no-interaction --verbose $@'
 )
-# in case you need it one day:
-# && $GOSU_CMD sh -c 'COMPOSER_MEMORY_LIMIT=-1 php -d memory_limit=-1 /usr/local/bin/composer install --prefer-dist --optimize-autoloader --no-interaction --verbose $@'
-
